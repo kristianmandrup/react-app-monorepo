165ec83e4fc6df179d796babedc643fc9106202b
Kristian Mandrup <kmandrup@gmail.com>
2018-08-27 17:10:21 +0700

    fix component refs in Product Static

--- packages/product/app/src/App/variant/Static/Material.tsx
+++ packages/product/app/src/App/variant/Static/Material.tsx
@@ -1,4 +1,4 @@
 import React from 'react'
 import {Static} from './Component'
 
-export const Material = (props : any) => (<Static.Material {...props}/>)
+export const Material = (props: any) => (<Static.Display.Material {...props}/>)
--- packages/product/app/src/App/variant/Static/Plain.tsx
+++ packages/product/app/src/App/variant/Static/Plain.tsx
@@ -1,4 +1,4 @@
 import React from 'react'
 import {Static} from './Component'
 
-export const Plain = (props : any) => (<Static.Plain {...props}/>)
+export const Plain = (props: any) => (<Static.Display.Plain {...props}/>)
